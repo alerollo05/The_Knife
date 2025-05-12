1768105ca574d3f66b945fa38deb987b11c74ae8
feffo32 <fedemarialux@gmail.com>
2025-05-12 22:25:54 +0200

    Update prova.java

--- prova.java
+++ prova.java
@@ -8,38 +8,41 @@ public class prova {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 try {
-	            ObjectMapper mapper = new ObjectMapper();
-	            RistorantiWrapper data = mapper.readValue(new File("prova.json"), RistorantiWrapper.class);
+		// Crea il nuovo oggetto ristorante
+    	            Ristorante nuovo = new Ristorante();
+    	            
+    	            nuovo.Id = 1;
+    	            nuovo.Name = "PEPO";
+    	            nuovo.Address = "Via Nuova 123, Roma, Italia";
+    	            nuovo.Location = "Rome, Italy";
+    	            nuovo.Price = "€€";
+    	            nuovo.Cuisine = "Italian";
+    	            nuovo.Longitude = 12.4964;
+    	            nuovo.Latitude = 41.9028;
+    	            nuovo.PhoneNumber = "390612345678";
+    	            nuovo.Url = "https://example.com";
+    	            nuovo.WebsiteUrl = "https://nuovoristorante.it";
+    	            nuovo.Award = "1 Star";
+    	            nuovo.GreenStar = 1;
+    	            nuovo.FacilitiesAndServices = "Air conditioning, Terrace";
+    	            nuovo.Description = "Un accogliente ristorante italiano nel cuore di Roma.";
+    	            nuovo.Delivery = true;
+    	            
+    	            GestioneRistoranti.aggiungiRistorante(nuovo, "prova.json");
+    	            GestioneRistoranti.rimuoviRistorantePerNome("Steirereck im Stadtpark", "prova.json");
+    	            
+    	            
+    	            /*ObjectMapper mapper = new ObjectMapper();
+    	            File file = new File("prova.json");
 
-	            for (Ristorante ristorante : data.ristoranti) {
-	                System.out.println(ristorante.Name + " - " + ristorante.Location + " - " + ristorante.PhoneNumber);
-	            }
-	            
-	            // Crea nuovo ristorante
-	            Ristorante nuovo = new Ristorante();
-	            
-	            System.out.println();
-	            nuovo.Name = "Nuovo Ristorante";
-	            nuovo.Address = "Via Nuova 123, Roma, Italia";
-	            nuovo.Location = "Rome, Italy";
-	            nuovo.Price = "€€";
-	            nuovo.Cuisine = "Italian";
-	            nuovo.Longitude = 12.4964;
-	            nuovo.Latitude = 41.9028;
-	            nuovo.PhoneNumber = 390612345678.0;
-	            nuovo.Url = "https://example.com";
-	            nuovo.WebsiteUrl = "https://nuovoristorante.it";
-	            nuovo.Award = "1 Star";
-	            nuovo.GreenStar = 1;
-	            nuovo.FacilitiesAndServices = "Air conditioning, Terrace";
-	            nuovo.Description = "Un accogliente ristorante italiano nel cuore di Roma.";
+    	            // Leggi i dati esistenti
+    	            RistorantiWrapper data = mapper.readValue(file, RistorantiWrapper.class);
 
-	            // Aggiungi alla lista
-	            data.ristoranti.add(nuovo);
+    	            for (Ristorante ristorante : data.ristoranti) {
+    	                System.out.println(ristorante.Name + " - " + ristorante.Location + " - " + ristorante.PhoneNumber);
+    	            }*/
 
-	            // Scrivi di nuovo nel file
-	            mapper.writerWithDefaultPrettyPrinter().writeValue(new File ("prova.json"), data);
+
 
 	        } catch (Exception e) {
 	            e.printStackTrace();
