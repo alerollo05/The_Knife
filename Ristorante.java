8687875b83f909ab89a0d83930876af3f2883e59
feffo32 <fedemarialux@gmail.com>
2025-05-12 22:23:34 +0200

    Update Ristorante.java

--- Ristorante.java
+++ Ristorante.java
@@ -1,19 +1,21 @@
 package jsonLibreriaJackson;
 
 public class Ristorante {
-	    public String Name;
-	    public String Address;
-	    public String Location;
-	    public String Price;
-	    public String Cuisine;
-	    public double Longitude;
-	    public double Latitude;
-	    public double PhoneNumber;
-	    public String Url;
-	    public String WebsiteUrl;
-	    public String Award;
-	    public int GreenStar;
-	    public String FacilitiesAndServices;
-	    public String Description;
+	    public int Id;
+    public String Name;
+    public String Address;
+    public String Location;
+    public String Price;
+    public String Cuisine;
+    public double Longitude;
+    public double Latitude;
+    public String PhoneNumber;
+    public String Url;
+    public String WebsiteUrl;
+    public String Award;
+    public int GreenStar;
+    public String FacilitiesAndServices;
+    public String Description;
+    public boolean Delivery;
 
 }
