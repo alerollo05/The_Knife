304b6735cb7a861137ce51f7b9d368bd88aca372
feffo32 <fedemarialux@gmail.com>
2025-05-28 19:59:37 +0200

    aggiunta get utente

--- ProfilePageClientController.java
+++ ProfilePageClientController.java
@@ -1,11 +1,22 @@
 package com.example.the_knife.Cliente;
 
+import com.example.the_knife.Exceptions.UtenteException;
+import com.example.the_knife.Ristoratore.Ristorante;
+import com.example.the_knife.Utente.ListaUtenti;
 import com.example.the_knife.Utente.SessionManager;
+import com.example.the_knife.Utente.Utente;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ProfilePageClientController extends dashBoardClientController {
     @FXML
@@ -45,5 +56,31 @@ public class ProfilePageClientController extends dashBoardClientController {
     @FXML
     protected void onRistorantiClick(ActionEvent event) throws IOException {
         super.onRistorantiClick(event);
+    }
+
+    private Utente getUtente(String fileJson){
+        try {
+
+            ObjectMapper mapper = new ObjectMapper();// Crea un'istanza di ObjectMapper
+            mapper.registerModule(new com.fasterxml.jackson.datatype.jsr310.JavaTimeModule());
+            File file = new File(fileJson);// Crea un oggetto File per rappresentare il file JSON passato come parametro
+
+            if (!file.exists()) {
+                System.out.println("File non trovato: " + file.getAbsolutePath());
+                return null;
+            }
+
+            ListaUtenti lista = mapper.readValue(new File(fileJson), ListaUtenti.class);
+
+            for(Utente u : lista.Utenti){
+                if(u.Username.equalsIgnoreCase(user)){
+                    return u;
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
